5a7e66826d3f06bc88a75c4e03bbfc68f1edc87e
Jorge Braz de Souza Filho <jorge.not@gmail.com>
2023-11-17 12:41:11 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: #B6B6B6;
+    width: 200px;
+    height: 200px;
+    border: 1px solid #4F4F4FB2;
+    border-radius: 10px;
+    font-size: 50px;
+    font-family: roboto;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 200px;
+    background-color: #aaffaa;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 28px;
+    font-family: roboto;
+    
+
+
+    input {
+        width: 100%;
+        height: 200px;
+        background-color: #aaffaa;
+        border: 0;
+        float: right;
+        font-size: 28px;
+        font-family: roboto;
+        display: flex;
+        align-items: flex-end;
+        text-align: right;
+        padding: 0 10px;
+        color: #6A6A6A;
+    }
+`
